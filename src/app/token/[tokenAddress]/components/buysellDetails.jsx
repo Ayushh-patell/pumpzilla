a7a4d00cbe4caeed64489a976e1d3b3d7445795e
Ayushh-patell <ayush.patel.code@gmail.com>
2024-10-24 15:12:34 +0530

    more changes with the token screen mobile

--- src/app/token/[tokenAddress]/components/buysellDetails.jsx
+++ src/app/token/[tokenAddress]/components/buysellDetails.jsx
@@ -36,8 +36,8 @@ const BuySellDetails = ({tokenData}) => {
 <div className="detailDiv w-full lg890md:rounded-3xl rounded-2xl bg-darkPry p-4 relative overflow-hidden md:h-[55%]">
     {/* BUY/SELL BUTTON */}
        <div className=' flex justify-center items-center gap-4 relative z-10 lg890md:text-base text-sm'>
-        <button onClick={() => setisBuy(true)} className={`${isBuy? " text-white bg-gradient-to-r from-bluePry to-[#5E6EFF]":" text-white/30 bg-[#2C2D30]"} rounded-2xl ${mont.className} font-extrabold lg890md:py-2 py-1 lg890md:px-4 px-2 w-1/2`} style={{boxShadow:"4px 4px 10px rgba(0,0,0,0.4)"}}>BUY</button>
-        <button onClick={() => setisBuy(false)} className={`${!isBuy? " text-white bg-gradient-to-r from-bluePry to-[#5E6EFF]":" text-white/30 bg-[#2C2D30]"} rounded-2xl ${mont.className} font-extrabold lg890md:py-2 py-1 lg890md:px-4 px-2 w-1/2`} style={{boxShadow:"4px 4px 10px rgba(0,0,0,0.4)"}}>SELL</button>
+        <button onClick={() => setisBuy(true)} className={`${isBuy? " text-white bg-gradient-to-r from-bluePry to-[#5E6EFF]":" text-white/30 bg-[#2C2D30]"} rounded-2xl ${mont.className} font-extrabold lg890md:py-2 md:py-1 py-2 lg890md:px-4 px-2 md:w-1/2 w-1/3`} style={{boxShadow:"4px 4px 10px rgba(0,0,0,0.4)"}}>BUY</button>
+        <button onClick={() => setisBuy(false)} className={`${!isBuy? " text-white bg-gradient-to-r from-bluePry to-[#5E6EFF]":" text-white/30 bg-[#2C2D30]"} rounded-2xl ${mont.className} font-extrabold lg890md:py-2 md:py-1 py-2 lg890md:px-4 px-2 md:w-1/2 w-1/3`} style={{boxShadow:"4px 4px 10px rgba(0,0,0,0.4)"}}>SELL</button>
        </div> 
        {/* SETTING */}
        <div className=' flex justify-end items-center py-3'>
@@ -61,8 +61,8 @@ const BuySellDetails = ({tokenData}) => {
       </div>
       {/* INFO */}
       <div className={` w-full rounded-3xl lg:text-base text-xs xl:p-4 p-2 bg-blackPry ${bebas.className} relative z-10 text-center`}>
-      <p>There are <span className=' bg-gradient-to-r from-[#646EFF] to-[#B471FF] bg-clip-text text-transparent'>676,991,162.93</span> PROMOTION tokens and <span className=' bg-gradient-to-r from-[#525dff] to-[#535fff] bg-clip-text text-transparent'>4,546.3</span> ETH still available in the bonding curve for sale.</p>
-      <p> When the <span className=' bg-gradient-to-r from-[#646EFF] to-[#9570FF] bg-clip-text text-transparent'>Market cap</span> reaches <span className=' bg-gradient-to-r lg:from-[#B872FF] from-[#3e4bff] lg:to-[#B871FE] to-[#646EFF] bg-clip-text text-transparent'>$77,296.81</span>, all the liquidity from the bonding curve will be moved to UNISWAP and <span className=' bg-gradient-to-r from-[#646EFF] to-[#8F6FFE] bg-clip-text text-transparent'>locked</span>. <span className=' bg-gradient-to-r from-[#FF4A6A] to-[#E40CC9] bg-clip-text text-transparent'>The process speeds up as the price increases</span>.</p>
+      <p>There are <span className=' bg-gradient-to-r from-[#646EFF] to-[#B471FF] bg-clip-text text-transparent'>676,991,162.93</span> PROMOTION tokens and <span className=' bg-gradient-to-r sm450:from-[#525dff] from-[#A264E2] sm450:to-[#535fff] to-[#A365E1] bg-clip-text text-transparent'>4,546.3</span> ETH still available in the bonding curve for sale.</p>
+      <p> When the <span className=' bg-gradient-to-r from-[#646EFF] to-[#9570FF] bg-clip-text text-transparent'>Market cap</span> reaches <span className=' bg-gradient-to-r lg:from-[#B872FF] sm450:from-[#3e4bff] lg:to-[#B871FE] sm450:to-[#646EFF] from-[#646EFF] to-[#A365E1] bg-clip-text text-transparent'>$77,296.81</span>, all the liquidity from the bonding curve will be moved to UNISWAP and <span className=' bg-gradient-to-r from-[#646EFF] to-[#8F6FFE] bg-clip-text text-transparent'>locked</span>. <span className=' bg-gradient-to-r from-[#FF4A6A] to-[#E40CC9] bg-clip-text text-transparent'>The process speeds up as the price increases</span>.</p>
       </div>
       {/* FINAL BUTTON */}
     <div className=' w-full flex justify-center items-center py-2'>
@@ -75,13 +75,13 @@ const BuySellDetails = ({tokenData}) => {
      <div id='SettingsModalWrapper' className=' hidden pointer-events-none font-beat fixed z-50 top-0 left-0 w-screen h-dvh justify-center items-center bg-black/70'>
       <div id='SettingsModal' className=' scale-50 lg890md:w-1/2 w-3/4 rounded-3xl border-2 border-black bg-[#24262B] relative p-4'>
         <div className=' relative w-full flex justify-between items-center'>
-          <p className=' relative z-10 bg-gradient-to-r from-[#5E6EFF] to-bluePry bg-clip-text text-transparent font-beat text-3xl'>SET MAX SLIPPAGE %</p>
-          <p id='slippagetextBorder' className=' absolute z-0 top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-[#5E6EFF] to-bluePry bg-clip-text text-transparent font-beat text-3xl pointer-events-none'>SET MAX SLIPPAGE %</p>
+          <p className=' relative z-10 bg-gradient-to-r from-[#5E6EFF] to-bluePry bg-clip-text text-transparent font-beat md:text-3xl text-xl'>SET MAX SLIPPAGE %</p>
+          <p id='slippagetextBorder' className=' absolute z-0 top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-[#5E6EFF] to-bluePry bg-clip-text text-transparent font-beat md:text-3xl text-xl pointer-events-none'>SET MAX SLIPPAGE %</p>
 
           <button id='closeModalBtn' onClick={closeModal} className=' text-white flex justify-center items-center'><Image src={'/close.png'} alt='close' width={20} height={20} className=' size-6'/></button>
         </div>
 
-        <div className=' w-full grid grid-cols-4 gap-4 text-2xl mt-3 relative z-10'>
+        <div className=' w-full grid sm:grid-cols-4 grid-cols-3 gap-4 text-2xl mt-3 relative z-10'>
           <button onClick={() => setslippage("2")} className={` ${slippage==='2'?" text-white ":" text-white/20"} relative flex justify-center items-center px-4 py-2 rounded-2xl overflow-hidden bg-[#161616]`} style={{boxShadow:"5px 5px 10px rgba(0,0,0,0.4)"}}>
             <div className={` pointer-events-none absolute z-0 top-0 left-0 w-full h-full bg-gradient-to-b from-bluePry to-[#5E6EFF] ${slippage==="2"?" opacity-100":" opacity-0"} transition-all duration-200 ease-in-out`}></div>
             <p className=' relative z-10'>2%</p>
@@ -94,11 +94,11 @@ const BuySellDetails = ({tokenData}) => {
             <div className={` pointer-events-none absolute z-0 top-0 left-0 w-full h-full bg-gradient-to-b from-bluePry to-[#5E6EFF] ${slippage==="20"?" opacity-100":" opacity-0"} transition-all duration-200 ease-in-out`}></div>
             <p className=' relative z-10'>20%</p>
           </button>
-          <input onChange={changeSlippage} type="text" placeholder='ENTER AMOUNT' value={slippage?slippage:""} className=' w-full bg-[#161616] rounded-2xl px-4 py-1 placeholder:text-white/20 text-white' />
+          <input onChange={changeSlippage} type="text" placeholder='ENTER AMOUNT' value={slippage?slippage:""} className=' sm:col-span-1 col-span-3 w-full bg-[#161616] rounded-2xl px-4 py-1 placeholder:text-white/20 text-white' />
         </div>
 
         <div className={`${(slippage>=10)?" opacity-100":" opacity-0"} transition-all duration-150 w-full p-4`}>
-          <p className={`${bebas.className} text-[#FF2F61] w-full text-center`}>Caution: Slippage above 10% may result in significant deviations in the transaction price. Please proceed with caution.</p>
+          <p className={`${bebas.className} text-[#FF2F61] w-full text-center md:text-base text-xs`}>Caution: Slippage above 10% may result in significant deviations in the transaction price. Please proceed with caution.</p>
         </div>
 
       <div className=' w-full flex justify-center items-center'>

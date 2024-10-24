545edbcf948c0d125110dc7b4675dbbc34deb176
Ayushh-patell <ayush.patel.code@gmail.com>
2024-10-24 12:29:45 +0530

    home nav opoup change for mobile

--- src/app/components/HomeNav.jsx
+++ src/app/components/HomeNav.jsx
@@ -49,7 +49,7 @@ const HomeNav = () => {
             else {
               let launchIcon = document.getElementById('launchIcon').getBoundingClientRect();
               let rightPercent = ((launchIcon.right/window.innerWidth)*100).toFixed(1)
-              gsap.to('#navMovingBar', {right:`${99-rightPercent}%`, opacity:1});
+              gsap.to('#navMovingBar', {right:`${98.6-rightPercent}%`, opacity:1});
             }
           }
         }
@@ -63,26 +63,30 @@ const HomeNav = () => {
         <Image src={'/home-active.png'} width={30} height={30} className={` sm:size-5 size-4 absolute z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ${currNav===0?" opacity-100":" opacity-0"} transition-all duration-200 `} alt="Home"/>
 
         {/* POPUP */}
-        <div className=' absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Home</div>
+        <div className=' md:block hidden absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Home</div>
+        <div className=' md:hidden block absolute bottom-full left-1/2 px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-y-0 -translate-x-1/2 group-hover/nav:opacity-100 group-hover/nav:-translate-y-2 transition-all duration-300 ease-out'>Home</div>
     </div>
     <div onClick={()=>{setcurrNav(1);router.push('/?launch=true')}} className={` cursor-pointer h-8 w-full flex justify-center items-center relative group/nav`}>
         <Image id='launchIcon' src={'/Launch.png'} width={30} height={30} className={` sm:size-5 size-4 ${currNav===0 || currNav===2?" opacity-100":" opacity-0"} transition-all duration-200  `} alt="Launch"/>
         <Image src={'/launch-active.png'} width={30} height={30} className={` sm:size-5 size-4 absolute z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 ${currNav===1?" opacity-100":" opacity-0"} transition-all duration-200 `} alt="Launch"/>
 
         {/* POPUP */}
-        <div className=' absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Launch</div>
+        <div className=' md:block hidden absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Launch</div>
+        <div className=' md:hidden block absolute bottom-full left-1/2 px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-y-0 -translate-x-1/2 group-hover/nav:opacity-100 group-hover/nav:-translate-y-2 transition-all duration-300 ease-out'>Launch</div>
     </div>
     <div className={` cursor-pointer h-8 w-full flex justify-center items-center group/nav relative`}>
         <Image src={'/twitter.png'} width={30} height={30} className=" sm:size-5 size-4" alt="Twitter"/>
 
         {/* POPUP */}
-        <div className=' absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Twitter</div>
+        <div className=' md:block hidden absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Twitter</div>
+        <div className=' md:hidden block absolute bottom-full left-1/2 px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-y-0 -translate-x-1/2 group-hover/nav:opacity-100 group-hover/nav:-translate-y-2 transition-all duration-300 ease-out'>Twitter</div>
     </div>
     <div className={` cursor-pointer h-8 w-full flex justify-center items-center group/nav relative`}>
         <Image src={'/telegram.png'} width={30} height={30} className=" sm:size-5 size-4" alt="Telegram"/>
 
         {/* POPUP */}
-        <div className=' absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Telegram</div>
+        <div className=' md:block hidden absolute top-1/2 left-full px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-x-0 -translate-y-1/2 group-hover/nav:opacity-100 group-hover/nav:translate-x-2 transition-all duration-300 ease-out'>Telegram</div>
+        <div className=' md:hidden block absolute bottom-full left-1/2 px-2 py-1 rounded-md font-bold text-[10px] pointer-events-none bg-[#1B1C1E] text-white opacity-0 translate-y-0 -translate-x-1/2 group-hover/nav:opacity-100 group-hover/nav:-translate-y-2 transition-all duration-300 ease-out'>Telegram</div>
     </div>
 
 
